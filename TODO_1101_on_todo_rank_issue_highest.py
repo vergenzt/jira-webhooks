ce44d61bae838ecab147cb7bd9b9a1f35b53a989
Tim Vergenz <vergenzt@gmail.com>
2018-05-17 12:17:54 -0700

    Fix top-ranking to parse body correctly

--- TODO_1101_on_todo_rank_issue_highest.py
+++ TODO_1101_on_todo_rank_issue_highest.py
@@ -1,4 +1,5 @@
 import os
+import json
 from jira_client import jira
 
 JQL_FOR_TOP_ISSUE = os.environ['JIRA_JQL_FOR_TOP_ISSUE']
@@ -8,7 +9,9 @@ def handler(event, context):
     1. Let top = [highest-ranked issue from epic](https://goo.gl/559GY4) in "To Do".
     2. [Rank trigger issue](https://goo.gl/KSxCRN) higher than top.
     """
-    trigger_issue_key = event['body']['issue']['key'] # https://goo.gl/fdk1wH
+    event_body = json.loads(event['body'])
+
+    trigger_issue_key = event_body['issue']['key'] # https://goo.gl/fdk1wH
     top_issues = jira.search_issues(JQL_FOR_TOP_ISSUE, maxResults=1)
     top_issue_key = top_issues[0].key
 
